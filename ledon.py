4ae74ce5b10d27c0d1e00730f37a638a23f6b112
ynakano1127 <yanakno1127@gmail.com>
2019-09-26 02:13:28 +0100

    [fix]えるちか

--- ledon.py
+++ ledon.py
@@ -4,7 +4,11 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
 
-GPIO.output(14, GPIO.HIGH)
-time.sleep(10)
+for i in range(10):
+    GPIO.output(14, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(14, GPIO.LOW)
+    time.sleep(1)
 
 GPIO.cleanup()
+
